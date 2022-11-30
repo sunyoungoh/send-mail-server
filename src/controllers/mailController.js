a528527d7d6df25f2789d58393e5f17586c141a4
sunyoungoh <tjsdud7379@gmail.com>
2022-11-30 19:55:41 +0900

    Add getFileOption function

--- src/controllers/mailController.js
+++ src/controllers/mailController.js
@@ -1,131 +1,92 @@
 import nodemailer from 'nodemailer';
 import { mailText } from '../mailTemplate.js';
 
+const getFileOption = itemOptionName => {
+  let fileOption = itemOptionName;
+  fileOption = fileOption.replace('화이트', 'White');
+  fileOption = fileOption.replace('다크', 'Dark');
+  fileOption = fileOption.replace('라이트', 'Light');
+  fileOption = fileOption.replace('인디핑크', 'IndiePink');
+  fileOption = fileOption.replace('스카이블루', 'SkyBlue');
+  fileOption = fileOption.replace('차콜', 'Charcoal');
+  fileOption = fileOption.replace('캔디핑크', 'CandyPink');
+  fileOption = fileOption.replace('스케줄', 'Schedule');
+  fileOption = fileOption.replace('타임테이블', 'TimeTable');
+  fileOption = fileOption.replace('월요일시작', 'Mon');
+  fileOption = fileOption.replace('일요일시작', 'Sun');
+  return fileOption;
+};
+
 const getItemInfo = (itemId, itemOptionName) => {
   let itemInfo = {};
   let fileName = '';
-  let fileOption = '';
+  let fileOption = getFileOption(itemOptionName);
 
-  if (itemOptionName == '화이트') {
-    fileOption = 'White';
-  } else if (itemOptionName == '다크') {
-    fileOption = 'Dark';
-  }
-
-  if (itemId == '5033569') {
+  if (itemId == 5033569) {
     itemInfo.itemName = '2023 심플 플래너';
-    if (itemOptionName == '라이트') {
-      fileOption = 'Light';
-    } else if (itemOptionName == '인디핑크') {
-      fileOption = 'IndiePink';
-    } else if (itemOptionName == '스카이블루') {
-      fileOption = 'SkyBlue';
-    }
     fileName = `2022+2023_Simple_Planner(${fileOption}).zip`;
   }
 
-  if (itemId == '5033568') {
+  if (itemId == 5033568) {
     itemInfo.itemName = '2023 모던 플래너';
     fileName = `2022+2023_Modern_Planner(${fileOption}).zip`;
   }
 
-  if (itemId == '5033567') {
+  if (itemId == 5033567) {
     itemInfo.itemName = '2023 타임라인 플래너';
-    if (itemOptionName == '스케줄') {
-      fileOption = 'Schedule';
-    } else if (itemOptionName == '타임테이블') {
-      fileOption = 'TimeTable';
-    }
     fileName = `2023_Timeline_Planner(${fileOption}).zip`;
   }
 
-  if (itemId == '5033566') {
+  if (itemId == 5033566) {
     itemInfo.itemName = '2023 먼슬리&데일리 플래너';
-    // 월요일 시작
-    if (itemOptionName == '화이트,월요일시작,타임테이블') {
-      fileOption = 'White,Mon,TimeTable';
-    } else if (itemOptionName == '화이트,월요일시작,스케줄') {
-      fileOption = 'White,Mon,Schedule';
-    } else if (itemOptionName == '다크,월요일시작,타임테이블') {
-      fileOption = 'Dark,Mon,TimeTable';
-    } else if (itemOptionName == '다크,월요일시작,스케줄') {
-      fileOption = 'Dark,Mon,Schedule';
-    }
-    // 일요일 시작
-    if (itemOptionName == '화이트,일요일시작,타임테이블') {
-      fileOption = 'White,Sun,TimeTable';
-    } else if (itemOptionName == '화이트,일요일시작,스케줄') {
-      fileOption = 'White,Sun,Schedule';
-    } else if (itemOptionName == '다크,일요일시작,타임테이블') {
-      fileOption = 'Dark,Sun,TimeTable';
-    } else if (itemOptionName == '다크,일요일시작,스케줄') {
-      fileOption = 'Dark,Sun,Schedule';
-    }
+
     fileName = `2023_Monthly+Daily_Planner(${fileOption}).zip`;
   }
 
-  if (itemId == '5033565') {
+  if (itemId == 5033565) {
     itemInfo.itemName = '31DAYS 플래너';
-    if (itemOptionName == '화이트,스케줄') {
-      fileOption = 'White,Schedule';
-    } else if (itemOptionName == '화이트,타임테이블') {
-      fileOption = 'White,TimeTable';
-    } else if (itemOptionName == '다크,스케줄') {
-      fileOption = 'Dark,Schedule';
-    } else if (itemOptionName == '다크,타임테이블') {
-      fileOption = 'Dark,TimeTable';
-    }
     fileName = `31DAYS_Planner(${fileOption}).zip`;
   }
 
-  if (itemId == '5033564') {
+  if (itemId == 5033564) {
     itemInfo.itemName = '세로형 인덱스 노트';
     fileName = `Index_Note(Vertical,${fileOption}).pdf`;
   }
 
-  if (itemId == '5033563') {
+  if (itemId == 5033563) {
     itemInfo.itemName = '가로형 인덱스 노트';
     fileName = `Index_Note(Horizontal,${fileOption}).pdf`;
   }
 
-  if (itemId == '5033560') {
+  if (itemId == 5033560) {
     itemInfo.itemName = '독서노트';
     fileName = `Reading_Journal(${fileOption}).zip`;
   }
 
-  if (itemId == '5033558') {
+  if (itemId == 5033558) {
     itemInfo.itemName = '드라마노트';
     fileName = `Drama_Journal(${fileOption}).zip`;
   }
 
-  if (itemId == '5033557') {
+  if (itemId == 5033557) {
     itemInfo.itemName = '먼슬리 트래커북';
-    if (itemOptionName == '차콜') {
-      fileOption = 'Charcoal';
-    } else if (itemOptionName == '캔디핑크') {
-      fileOption = 'CandyPink';
-    } else if (itemOptionName == '스카이블루') {
-      fileOption = 'SkyBlue';
-    }
     fileName = `12_Months_Goal_Tracker(${fileOption}).zip`;
   }
 
-  if (itemId == '5033561') {
+  if (itemId == 5033561) {
     itemInfo.itemName = '180 베이직 노트패드';
     fileName = `180_Basic_Notepad.zip`;
   }
 
-  if (itemId == '5033559') {
+  if (itemId == 5033559) {
     itemInfo.itemName = '180 체커보드 노트패드';
     fileName = `180_CheckerBoard_Notepad.zip`;
   }
 
-  if (itemId == '5033562') {
+  if (itemId == 5033562) {
     let itemOptionYears = itemOptionName.slice(0, 1);
     itemInfo.itemName = itemOptionYears == 3 ? '3년 일기' : '5년 일기';
-    // 3년, 5년 옵션 지우고 컬러 옵션만 표시
     itemOptionName = itemOptionName.split(',')[1];
-
     fileName = `${itemOptionYears}_Years_Diary(${fileOption}).zip`;
   }
 
