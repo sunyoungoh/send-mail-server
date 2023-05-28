77701f739ded6e29641cb72b14e38614dea13884
SunYoung Oh <tjsdud7379@gmail.com>
2023-05-28 23:10:41 +0900

    Fix bug

--- src/controllers/autoSend.js
+++ src/controllers/autoSend.js
@@ -132,7 +132,7 @@ export const tenbytenAutoSend = () => {
               dispatchData,
               {
                 headers: {
-                  Authorization: token,
+                  Authorization: encodeKey,
                 },
               }
             );
